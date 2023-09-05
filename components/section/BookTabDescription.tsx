b039999fadffb8d3795cb72ddf3662d709528603
Sam Makhlouf <maksam7dev@gmail.com>
2023-09-05 13:41:25 +0100

    fix

--- components/section/BookTabDescription.tsx
+++ components/section/BookTabDescription.tsx
@@ -4,7 +4,7 @@ export default function BookTabDescription() {
          <p className="leading-7 [&:not(:first-child)]:mt-6">
             What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
             printing and typesetting industry. Lorem Ipsum has been the
-            industry's standard dummy text ever since the 1500s, when an unknown
+            industrys standard dummy text ever since the 1500s, when an unknown
             printer took a galley of type and scrambled it to make a type
             specimen book. It has survived not only five centuries, but also the
             leap into electronic typesetting, remaining essentially unchanged.
