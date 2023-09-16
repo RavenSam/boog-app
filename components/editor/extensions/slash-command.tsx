9c9cabc22803118ff78c440e8e424380ec544512
Sam Makhlouf <61135465+RavenSam@users.noreply.github.com>
2023-09-16 14:47:13 +0100

    commented

--- components/editor/extensions/slash-command.tsx
+++ components/editor/extensions/slash-command.tsx
@@ -1,27 +1,29 @@
-import { Extension } from "@tiptap/core";
-import Suggestion from "@tiptap/suggestion";
+// import { Extension } from "@tiptap/core";
+// import Suggestion from "@tiptap/suggestion";
 
-export const Commands = Extension.create({
-  name: "commands",
+// export const Commands = Extension.create({
+//   name: "commands",
 
-  defaultOptions: {
-    suggestion: {
-      char: "/",
-      startOfLine: false,
-      command: ({ editor, range, props }) => {
-        props.command({ editor, range, props });
-      }
-    }
-  },
+//   defaultOptions: {
+//     suggestion: {
+//       char: "/",
+//       startOfLine: false,
+//       command: ({ editor, range, props }) => {
+//         props.command({ editor, range, props });
+//       }
+//     }
+//   },
 
-  addProseMirrorPlugins() {
-    return [
-      Suggestion({
-        editor: this.editor,
-        ...this.options.suggestion
-      })
-    ];
-  }
-});
+//   addProseMirrorPlugins() {
+//     return [
+//       Suggestion({
+//         editor: this.editor,
+//         ...this.options.suggestion
+//       })
+//     ];
+//   }
+// });
 
-
+export default function SlashCommand() {
+   return <div>slash-command</div>
+}
