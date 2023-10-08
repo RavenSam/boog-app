2a59af7c6c3a1cd97e3d982c91660205ba84a714
Sam Makhlouf <61135465+RavenSam@users.noreply.github.com>
2023-10-08 05:59:25 +0000

    Hide navbar in edit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,9 @@
 "use client"
+
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 import { cn } from "@/lib/utils"
-import { Settings, Search, LayoutDashboard, BookCopy, StickyNoteIcon } from "lucide-react"
+import { Settings, Search, LayoutDashboard, BookCopy, StickyNoteIcon, ChevronRight, ChevronLeft } from "lucide-react"
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Input } from "@/components/ui/input"
@@ -16,6 +18,7 @@ import {
    DropdownMenuSeparator,
    DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { useState } from "react"
 
 const navLinks = [
    { label: "Overview", icon: LayoutDashboard, href: "/author" },
@@ -24,17 +27,38 @@ const navLinks = [
 ]
 
 export default function Navbar() {
+   const pathname = usePathname()
+   const [openInEdit, setOpenInEdit] = useState(false)
+
+   const inEdit = pathname.split("/").includes("edit")
 
    return (
- 
-         <div className="w-16 fixed left-0 top-0 bottom-0">
+      <>
+         <div
+            className={cn(
+               "w-16 fixed left-0 top-0 bottom-0 border-r transition-all duration-300",
+               inEdit && !openInEdit && "-translate-x-16"
+            )}
+         >
+            <Button
+               onClick={() => setOpenInEdit((prev) => !prev)}
+               variant="secondary"
+               size="icon"
+               aria-label="show/hidde navbar"
+               className={cn(
+                  "absolute -right-8 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full transition-all duration-300",
+                  inEdit && !openInEdit && "-right-12 rotate-180 opacity-60  hover:opacity-100 hover:-right-14"
+               )}
+            >
+               <ChevronLeft className="h-4 w-4" />
+            </Button>
 
             <div className="flex flex-col items-center h-full py-4 px-2">
-                <Button variant="ghost" className="my-3" size="icon" aria-label="Search">
+               <Button variant="ghost" className="my-3" size="icon" aria-label="Search">
                   <Search className="h-5 w-5" />
-                </Button>
+               </Button>
 
-                <Separator/>
+               <Separator />
 
                {navLinks.map((item) => (
                   <Button variant="ghost" className="mt-3" key={item.label} size="icon" aria-label={item.label} asChild>
@@ -43,20 +67,19 @@ export default function Navbar() {
                      </Link>
                   </Button>
                ))}
-               
 
                <div className="mt-auto">
                   <UserNav />
                </div>
             </div>
          </div>
-   
+      </>
    )
 }
 
 function UserNav() {
    return (
-      <DropdownMenu > 
+      <DropdownMenu>
          <DropdownMenuTrigger asChild>
             <Button variant="ghost" className="relative h-10 w-10 rounded-full">
                <Avatar className="h-10 w-10">
