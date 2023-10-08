436660b7108fef15458d0089ca617c0573f822ec
Sam Makhlouf <61135465+RavenSam@users.noreply.github.com>
2023-10-08 06:13:36 +0000

    added tailwindcss prettier plugin

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,110 +1,134 @@
-"use client"
+"use client";
 
-import Link from "next/link"
-import { usePathname } from "next/navigation"
-import { cn } from "@/lib/utils"
-import { Settings, Search, LayoutDashboard, BookCopy, StickyNoteIcon, ChevronRight, ChevronLeft } from "lucide-react"
-
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-import { Separator } from "@/components/ui/separator"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 import {
-   DropdownMenu,
-   DropdownMenuContent,
-   DropdownMenuGroup,
-   DropdownMenuItem,
-   DropdownMenuLabel,
-   DropdownMenuSeparator,
-   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { useState } from "react"
+  Settings,
+  Search,
+  LayoutDashboard,
+  BookCopy,
+  StickyNoteIcon,
+  ChevronRight,
+  ChevronLeft,
+} from "lucide-react";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useState } from "react";
 
 const navLinks = [
-   { label: "Overview", icon: LayoutDashboard, href: "/author" },
-   { label: "Books", icon: BookCopy, href: "/author/books" },
-   { label: "Notes", icon: StickyNoteIcon, href: "/author/notes" },
-]
+  { label: "Overview", icon: LayoutDashboard, href: "/author" },
+  { label: "Books", icon: BookCopy, href: "/author/books" },
+  { label: "Notes", icon: StickyNoteIcon, href: "/author/notes" },
+];
 
 export default function Navbar() {
-   const pathname = usePathname()
-   const [openInEdit, setOpenInEdit] = useState(false)
+  const pathname = usePathname();
+  const [openInEdit, setOpenInEdit] = useState(false);
 
-   const inEdit = pathname.split("/").includes("edit")
+  const inEdit = pathname.split("/").includes("edit");
 
-   return (
-      <>
-         <div
-            className={cn(
-               "w-16 fixed left-0 top-0 bottom-0 border-r transition-all duration-300",
-               inEdit && !openInEdit && "-translate-x-16"
-            )}
-         >
+  return (
+    <>
+      <div
+        className={cn(
+          "fixed bottom-0 left-0 top-0 w-16 border-r transition-all duration-300",
+          inEdit && !openInEdit && "-translate-x-16",
+        )}
+      >
+        <Button
+          onClick={() => setOpenInEdit((prev) => !prev)}
+          variant="secondary"
+          size="icon"
+          aria-label="show/hidde navbar"
+          className={cn(
+            "absolute -right-8 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full transition-all duration-300",
+            inEdit &&
+              !openInEdit &&
+              "-right-12 rotate-180 opacity-60  hover:-right-14 hover:opacity-100",
+          )}
+        >
+          <ChevronLeft className="h-4 w-4" />
+        </Button>
+
+        <div className="flex h-full flex-col items-center px-2 py-4">
+          <Button
+            variant="ghost"
+            className="my-3"
+            size="icon"
+            aria-label="Search"
+          >
+            <Search className="h-5 w-5" />
+          </Button>
+
+          <Separator />
+
+          {navLinks.map((item) => (
             <Button
-               onClick={() => setOpenInEdit((prev) => !prev)}
-               variant="secondary"
-               size="icon"
-               aria-label="show/hidde navbar"
-               className={cn(
-                  "absolute -right-8 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-full transition-all duration-300",
-                  inEdit && !openInEdit && "-right-12 rotate-180 opacity-60  hover:opacity-100 hover:-right-14"
-               )}
+              variant="ghost"
+              className="mt-3"
+              key={item.label}
+              size="icon"
+              aria-label={item.label}
+              asChild
             >
-               <ChevronLeft className="h-4 w-4" />
+              <Link href={item.href}>
+                <item.icon className="h-5 w-5" />
+              </Link>
             </Button>
+          ))}
 
-            <div className="flex flex-col items-center h-full py-4 px-2">
-               <Button variant="ghost" className="my-3" size="icon" aria-label="Search">
-                  <Search className="h-5 w-5" />
-               </Button>
-
-               <Separator />
-
-               {navLinks.map((item) => (
-                  <Button variant="ghost" className="mt-3" key={item.label} size="icon" aria-label={item.label} asChild>
-                     <Link href={item.href}>
-                        <item.icon className="h-5 w-5" />
-                     </Link>
-                  </Button>
-               ))}
-
-               <div className="mt-auto">
-                  <UserNav />
-               </div>
-            </div>
-         </div>
-      </>
-   )
+          <div className="mt-auto">
+            <UserNav />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 function UserNav() {
-   return (
-      <DropdownMenu>
-         <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="relative h-10 w-10 rounded-full">
-               <Avatar className="h-10 w-10">
-                  <AvatarImage src="/avatars/01.png" alt="@john" />
-                  <AvatarFallback>JD</AvatarFallback>
-               </Avatar>
-            </Button>
-         </DropdownMenuTrigger>
-         <DropdownMenuContent side="right" className="w-56" align="end" forceMount>
-            <DropdownMenuLabel className="font-normal">
-               <div className="flex flex-col space-y-1">
-                  <p className="text-sm font-medium leading-none">john</p>
-                  <p className="text-xs leading-none text-muted-foreground">m@example.com</p>
-               </div>
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-               <DropdownMenuItem>Profile</DropdownMenuItem>
-               <DropdownMenuItem>Billing</DropdownMenuItem>
-               <DropdownMenuItem>Settings</DropdownMenuItem>
-               <DropdownMenuItem>New Team</DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Log out</DropdownMenuItem>
-         </DropdownMenuContent>
-      </DropdownMenu>
-   )
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative h-10 w-10 rounded-full">
+          <Avatar className="h-10 w-10">
+            <AvatarImage src="/avatars/01.png" alt="@john" />
+            <AvatarFallback>JD</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent side="right" className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">john</p>
+            <p className="text-xs leading-none text-muted-foreground">
+              m@example.com
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Settings</DropdownMenuItem>
+          <DropdownMenuItem>New Team</DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Log out</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
--- components/section/BooksSection.tsx
+++ components/section/BooksSection.tsx
@@ -1,77 +1,81 @@
-import Link from "next/link"
-import { Button } from "@/components/ui/button"
-import { ChevronRightIcon } from "@radix-ui/react-icons"
-import { Separator } from "@/components/ui/separator"
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
-import { Book, BookArtwork } from "@/components/custom-ui/BookCover"
-import BooksControl from "@/components/action/BooksControl"
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { ChevronRightIcon } from "@radix-ui/react-icons";
+import { Separator } from "@/components/ui/separator";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { Book, BookArtwork } from "@/components/custom-ui/BookCover";
+import BooksControl from "@/components/action/BooksControl";
 
 export default function BooksSection() {
-   return (
-      <>
-         <div className="">
-            <div className="bg-background space-y-6">
-               <BooksControl />
+  return (
+    <>
+      <div className="">
+        <div className="space-y-6 bg-background">
+          <BooksControl />
 
-               <Separator />
+          <Separator />
 
-               <div className="relative">
-                  <ScrollArea>
-                     <div className="flex space-x-4 pb-4">
-                        {RecentBook.map((book) => (
-                           <BookArtwork
-                              key={book.name}
-                              book={book}
-                              className="w-[250px]"
-                              aspectRatio="portrait"
-                              width={250}
-                              height={330}
-                           />
-                        ))}
+          <div className="relative">
+            <ScrollArea>
+              <div className="flex space-x-4 pb-4">
+                {RecentBook.map((book) => (
+                  <BookArtwork
+                    key={book.name}
+                    book={book}
+                    className="w-[250px]"
+                    aspectRatio="portrait"
+                    width={250}
+                    height={330}
+                  />
+                ))}
 
-                        <div className="w-60 aspect-[3/4] rounded-md  flex items-center justify-center">
-                           <div className="">
-                              <Button variant="ghost" asChild>
-                                 <Link href="/author/books">
-                                    <span>See more</span>
-                                    <ChevronRightIcon className="ml-2 h-4 w-4" />
-                                 </Link>
-                              </Button>
-                           </div>
-                        </div>
-                     </div>
-                     <ScrollBar orientation="horizontal" />
-                  </ScrollArea>
-               </div>
-            </div>
-         </div>
-      </>
-   )
+                <div className="flex aspect-[3/4] w-60  items-center justify-center rounded-md">
+                  <div className="">
+                    <Button variant="ghost" asChild>
+                      <Link href="/author/books">
+                        <span>See more</span>
+                        <ChevronRightIcon className="ml-2 h-4 w-4" />
+                      </Link>
+                    </Button>
+                  </div>
+                </div>
+              </div>
+              <ScrollBar orientation="horizontal" />
+            </ScrollArea>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export const RecentBook: Book[] = [
-   {
-      id:1,
-      name: "React Rendezvous",
-      artist: "Ethan Byte",
-      cover: "https://images.unsplash.com/photo-1611348586804-61bf6c080437?w=300&dpr=2&q=80",
-   },
-   {
-      id:2,
-      name: "Async Awakenings",
-      artist: "Nina Netcode",
-      cover: "https://images.unsplash.com/photo-1468817814611-b7edf94b5d60?w=300&dpr=2&q=80",
-   },
-   {
-      id:3,
-      name: "The Art of Reusability",
-      artist: "Lena Logic",
-      cover: "https://images.unsplash.com/photo-1528143358888-6d3c7f67bd5d?w=300&dpr=2&q=80",
-   },
-   {
-      id:4,
-      name: "Stateful Symphony",
-      artist: "Beth Binary",
-      cover: "https://images.unsplash.com/photo-1490300472339-79e4adc6be4a?w=300&dpr=2&q=80",
-   },
-]
+  {
+    id: 1,
+    name: "React Rendezvous",
+    artist: "Ethan Byte",
+    cover:
+      "https://images.unsplash.com/photo-1611348586804-61bf6c080437?w=300&dpr=2&q=80",
+  },
+  {
+    id: 2,
+    name: "Async Awakenings",
+    artist: "Nina Netcode",
+    cover:
+      "https://images.unsplash.com/photo-1468817814611-b7edf94b5d60?w=300&dpr=2&q=80",
+  },
+  {
+    id: 3,
+    name: "The Art of Reusability",
+    artist: "Lena Logic",
+    cover:
+      "https://images.unsplash.com/photo-1528143358888-6d3c7f67bd5d?w=300&dpr=2&q=80",
+  },
+  {
+    id: 4,
+    name: "Stateful Symphony",
+    artist: "Beth Binary",
+    cover:
+      "https://images.unsplash.com/photo-1490300472339-79e4adc6be4a?w=300&dpr=2&q=80",
+  },
+];
